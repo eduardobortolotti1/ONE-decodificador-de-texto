dc715074cb4226d544329c5597cc61605adb04a9
eduardobortolotti1 <tiolollipop@gmail.com>
2024-07-17 23:48:41 -0300

    feat: Reworked encryption/decryption logic to make it easier to understand

--- index.js
+++ index.js
@@ -1,52 +1,29 @@
-const decryptionKeys = {
-    "e": "enter",
-    "i": "imes",
-    "a": "ai",
-    "o": "ober",
-    "u": "ufat"
-};
-
 function handleEncryptClick() {
     const input = document.querySelector("#input-text").value.toLowerCase();
-    var input_clean_array = input.split("");
-    var encrypted_string = input_clean_array;
-
-    input_clean_array.forEach((value, index) => {
-        for ([key, encrypted_word] of Object.entries(decryptionKeys)) {
-            if (value === key) {
-                encrypted_string[index] = encrypted_word
-            }
-        }
-    })
-    encrypted_string = encrypted_string.join("");
+    let encrypted_string = input
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
     document.querySelector("#output-text").value = encrypted_string;
     updateOutputArea();
 }
 
 function handleDecryptClick() {
     const input = document.querySelector("#input-text").value.toLowerCase();
-    var decrypted_string = input;
-    var search_result_index;
-    var match_found = true
-
-    while (match_found === true) {
-        match_found = false;
-
-        for ([key, encrypted_word] of Object.entries(decryptionKeys)) {
-            search_result_index = decrypted_string.search(encrypted_word);
-            if (search_result_index != -1) {
-                decrypted_string = decrypted_string.slice(0, search_result_index) + key + decrypted_string.slice(search_result_index + encrypted_word.length);
-                match_found = true;
-            }
-        }
-    }
-
+    let decrypted_string = input
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
     document.querySelector("#output-text").value = decrypted_string;
     updateOutputArea();
 }
 
 async function handleCopyClick() {
-    var content = document.querySelector("#output-text").value;
+    let content = document.querySelector("#output-text").value;
     await navigator.clipboard.writeText(content);
 }
 
